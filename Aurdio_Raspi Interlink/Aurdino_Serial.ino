1e745818892bf7d395b2391980aef18ea8425b96
12DILEEP <90190565+12DILEEP@users.noreply.github.com>
2024-04-23 00:31:02 +0530

    Update Aurdino_Serial.ino

--- Aurdio_Raspi Interlink/Aurdino_Serial.ino
+++ Aurdio_Raspi Interlink/Aurdino_Serial.ino
@@ -1,1 +1,18 @@
+/* 
+This code is used to test whether the serial communication is bridged or not. 
 
+*/
+
+
+
+
+void setup() {
+Serial.begin(115200);
+while (!Serial) {}
+}
+
+void loop() {
+Serial.println("Hello from Arduino");
+delay(1000);
+
+}
