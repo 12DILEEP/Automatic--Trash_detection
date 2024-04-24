6735e3c87f2a3efb4f1ed39c26fddcc81423e5f8
12DILEEP <90190565+12DILEEP@users.noreply.github.com>
2024-04-24 14:31:46 +0530

    Update Pi_interlink.py

--- Aurdio_Raspi Interlink/Pi_interlink.py
+++ Aurdio_Raspi Interlink/Pi_interlink.py
@@ -1,7 +1,25 @@
+""" 
+The below code is used to check whether the serial communication has been established or not between the aurdino & raspberry pi
 
+In raspi check whether the raspi has a permission for serial communication or not by simply typing  groups in terminal of raspi if there is dialout (it means connected to output port)
 
+  if not
+    TYPE "sudo adduser pi(username) dialout"
+
+To Know which port it is connected then type command " ls/dev/tty* " 
+
+"""
 import serial
 
 import time
 
-ser = serial.serial('/dev/ttyACM0',
+ser = serial.serial('/dev/ttyACM0',11520, time.out=1.0)
+time.sleep(3)
+
+ser.reset_input_buffer()       # erases the data if it sends the data before the code exutes.
+
+print("serial ok")
+
+ser.close()
+
+
