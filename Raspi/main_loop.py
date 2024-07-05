c40b98d526cb906217fd5213f7f01b8308839cb6
12DILEEP <90190565+12DILEEP@users.noreply.github.com>
2024-07-05 11:29:46 +0530

    Update main_loop.py

--- Raspi/main_loop.py
+++ Raspi/main_loop.py
@@ -1,3 +1,6 @@
+"""To run the main file the command to excute is python main.py --modeldir ssd_mobilenet_v2_coco/saved_model --graph detect.tflite --labels labelmap.txt --threshold 0.5 --resolution 1280x720 --edgetpu
+ """
+
 from object_detection import ObjectDetection
 from stepper_rotation import StepperMotor
 import time
@@ -11,7 +14,7 @@ try:
         scores, classes = object_detection.detect_objects()
 
         # Check if any detection is above 90% confidence
-        if scores is not None and any(score > 0.9 for score in scores):
+        if scores is not None and any(score > 0.8 for score in scores):
             print("Object detected with high confidence. Stopping motor.")
             break
 
